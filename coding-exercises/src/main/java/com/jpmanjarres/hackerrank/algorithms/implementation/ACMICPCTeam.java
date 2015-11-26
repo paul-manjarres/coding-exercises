9874e7587e6c14d5e693a524e257dd7f6969249c
Jean Paul Manjarres Correal <jeanpaul.manjarres@pagosonline.com>
2015-11-26 18:05:59 -0500

    Added more exercises solved

---
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/implementation/ACMICPCTeam.java
@@ -0,0 +1,49 @@
+package com.jpmanjarres.hackerrank.algorithms.implementation;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * @author <a href="mailto:paul.manjarres@gmail.com">Jean Paul Manjarres Correal. </a> 26/11/2015
+ *
+ */
+public class ACMICPCTeam {
+
+	public static void main(String[] args) {
+		final Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+		int m = in.nextInt();
+
+		BigInteger[] persons = new BigInteger[n];
+
+		for (int i = 0; i < n; i++) {
+			String s = in.next();
+			persons[i] = new BigInteger(s, 2);
+		}
+
+		int maxTopics = 0;
+		int teams = 0;
+
+		for (int i = 0; i < persons.length; i++) {
+			for (int j = i+1; j < persons.length; j++) {
+
+				BigInteger val = persons[i].or(persons[j]);
+				int topics = val.bitCount();
+
+				if(topics>maxTopics){
+					maxTopics = topics;
+					teams=1;
+				}else if(topics == maxTopics){
+					teams++;
+				}
+			}
+
+		}
+
+		System.out.println(maxTopics);
+		System.out.println(teams);
+
+		in.close();
+	}
+}
---
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/implementation/LibraryFine.java
@@ -0,0 +1,39 @@
+package com.jpmanjarres.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+/**
+ * @author <a href="mailto:paul.manjarres@gmail.com">Jean Paul Manjarres Correal. </a> 26/11/2015
+ *
+ */
+public class LibraryFine {
+
+	public static void main(String[] args) {
+		final Scanner in = new Scanner(System.in);
+
+		int da = in.nextInt();
+		int ma = in.nextInt();
+		int ya = in.nextInt();
+		int de = in.nextInt();
+		int me = in.nextInt();
+		int ye = in.nextInt();
+
+		int fine = 0;
+
+		if (ya > ye) {
+			fine = 10000;
+		} else if(ya < ye){
+			fine = 0;
+		}else if (ma > me) {
+			fine = 500 * (ma - me);
+		} else if(ma < me) {
+			fine = 0;
+		}else {
+			fine = 15 * (da - de);
+			fine = fine < 0 ? 0 : fine;
+		}
+
+		System.out.println(fine);
+		in.close();
+	}
+}

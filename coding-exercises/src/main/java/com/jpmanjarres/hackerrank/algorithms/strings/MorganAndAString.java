4ccd09dd788cb1c9090f40944b34d91c2f8e28e9
Jean Paul Manjarres Correal <jeanpaul.manjarres@pagosonline.com>
2015-12-02 18:06:35 -0500

    MorganAndString small refactor

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/MorganAndAString.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/MorganAndAString.java
@@ -16,7 +16,7 @@ public class MorganAndAString {
 		int sumA = 0;
 		int sumB = 0;
 
-		while (k < B.length - 1 || j < A.length) {
+		while (k < B.length  || j < A.length) {
 			sumB += k < B.length ? (int) B[k] : (int) 'Z';
 			sumA += j < A.length ? (int) A[j] : (int) 'Z';
 			if (sumA == sumB) {
@@ -29,6 +29,30 @@ public class MorganAndAString {
 		return 0;
 	}
 
+	// Solve the problem for two arrays
+	public static String solve(char[] A,char[] B){
+		StringBuilder s = new StringBuilder();
+
+		int j = 0;
+		int k = 0;
+		int lengthSum = A.length + B.length;
+
+		while (j + k < lengthSum) {
+
+			if (j >= A.length) {
+				s.append(B[k++]);
+			} else if (k >= B.length) {
+				s.append(A[j++]);
+			} else if (A[j] == B[k]) {
+				s.append(checkWhosNext(A, j, B, k) == 0 ? A[j++] : B[k++]);
+			} else {
+				s.append(A[j] < B[k] ? A[j++] : B[k++]);
+			}
+		}
+		return s.toString();
+	}
+
+
 	public static void main(String[] args) {
 		final Scanner in = new Scanner(System.in);
 		int T = in.nextInt();
@@ -39,33 +63,11 @@ public class MorganAndAString {
 		for (int i = 0; i < T; i++) {
 			char[] A = in.next().toCharArray();
 			char[] B = in.next().toCharArray();
-
-			StringBuilder s = new StringBuilder();
-
-			int j = 0;
-			int k = 0;
-			int lengthSum = A.length + B.length;
-
-			while (j + k < lengthSum) {
-
-				if (j >= A.length) {
-					s.append(B[k++]);
-				} else if (k >= B.length) {
-					s.append(A[j++]);
-				} else if (A[j] == B[k]) {
-					s.append(checkWhosNext(A, j, B, k) == 0 ? A[j++] : B[k++]);
-				}
-				else if (A[j] < B[k]) {
-					s.append(A[j++]);
-				} else if (A[j] > B[k]) {
-					s.append(B[k++]);
-				}
-			}
-			System.out.println(s.toString());
-
+			System.out.println(solve(A, B));
 		}
+
 		end = System.currentTimeMillis();
-//		System.out.println("Time : " + (end - start));
+		System.out.println("Time : " + (end - start));
 		in.close();
 	}
 

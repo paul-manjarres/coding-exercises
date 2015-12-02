0d6122624d1777e7d0df3a9dfeba90d3256b9399
Jean Paul Manjarres Correal <jeanpaul.manjarres@pagosonline.com>
2015-12-02 10:32:52 -0500

    Solution working but timeout problems

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/MorganAndAString.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/MorganAndAString.java
@@ -3,6 +3,7 @@
  */
 package com.jpmanjarres.hackerrank.algorithms.strings;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,6 +11,27 @@ import java.util.Scanner;
  *
  */
 public class MorganAndAString {
+
+	// Check who is the array to take elements from if the two elements on top are equal.
+	public static int checkWhosNext(char[] A, char[] B) {
+		for (int i = 0;; i++) {
+			if (i <= A.length - 1 && i <= B.length - 1) {
+				if (A[i] < B[i]) {
+					return 0;
+				} else if (A[i] > B[i]) {
+					return 1;
+				} else {
+					// Do nothing
+				}
+			} else if (i < A.length - 1 && i >= B.length - 1) {
+				return 0;
+			} else if (i >= A.length - 1 && i < B.length - 1) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		final Scanner in = new Scanner(System.in);
@@ -19,58 +41,37 @@ public class MorganAndAString {
 			char[] A = in.next().toCharArray();
 			char[] B = in.next().toCharArray();
 
-			int j = 0;
-			int k = 0;
-
 			StringBuilder s = new StringBuilder();
 
+			int j = 0;
+			int k = 0;
 			while (j + k < A.length + B.length) {
 
 				if (j >= A.length) {
+					s.append(B[k]);
+					k++;
+				} else if (k >= B.length) {
+					s.append(A[j]);
+					j++;
+				} else if (A[j] < B[k]) {
+					s.append(A[j]);
+					j++;
+				} else if (A[j] > B[k]) {
+					s.append(B[k]);
+					k++;
+				} else if (A[j] == B[k]) {
 
-					if (k + 1 < B.length) {
+					int direction = checkWhosNext(Arrays.copyOfRange(A, j, A.length), Arrays.copyOfRange(B, k, B.length));
 
-						if (B[k] <= B[k + 1]) {
-							s.append(B[k]);
-							s.append(B[k + 1]);
-						} else {
-							s.append(B[k + 1]);
-							s.append(B[k]);
-						}
-						k += 2;
-					}else{
+					if (direction == 0) {
+						s.append(A[j]);
+						j++;
+					}
+					if (direction == 1) {
 						s.append(B[k]);
 						k++;
 					}
 
-
-
-				} else if (k >= B.length) {
-
-					if (j + 1 < A.length) {
-
-						if (A[j] <= A[j + 1]) {
-							s.append(A[j]);
-							s.append(A[j + 1]);
-						} else {
-							s.append(A[j + 1]);
-							s.append(A[j]);
-						}
-						j += 2;
-					}else{
-						s.append(A[j]);
-						j++;
-					}
-
-//					s.append(A[j]);
-//					j++;
-
-				} else if (A[j] <= B[k]) {
-					s.append(A[j]);
-					j++;
-				} else {
-					s.append(B[k]);
-					k++;
 				}
 			}
 			System.out.println(s.toString());

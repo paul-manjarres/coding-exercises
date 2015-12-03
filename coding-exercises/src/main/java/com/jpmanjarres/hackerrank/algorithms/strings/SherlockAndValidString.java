41ba6b8966a3ebc79edb405e2d8617ff323dce7e
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2015-12-02 22:02:06 -0500

    Sherlock And Valid String initial commit

---
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/SherlockAndValidString.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package com.jpmanjarres.hackerrank.algorithms.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author <a href="paul.manjarres@gmail.com">Jean Paul Manjarres Correal</a> 2/12/2015
+ *
+ */
+public class SherlockAndValidString {
+
+	public static void main(String[] args) {
+		final Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		for (int i = 0; i < N; i++) {
+
+		}
+		in.close();
+	}
+
+}

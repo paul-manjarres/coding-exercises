a74700a828fb6b9d65b56fb4fcc96cbe124dc8c0
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2016-02-27 16:56:45 -0500

    First version of the algorithm, just rotates the outside layer.

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/implementation/AlgoMatrixRotation.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/implementation/AlgoMatrixRotation.java
@@ -26,37 +26,77 @@ public class AlgoMatrixRotation {
 			}
 		}
 
-		int layers = M<N ? M/2 : N/2;
+		int layers = M < N ? M / 2 : N / 2;
 
-		System.out.println("Layers : "+layers);
+		System.out.println("Layers : " + layers);
 
+		// for (int k = 0; k < layers; k++) {
 
-		for(int i=0; i< layers; i++){
+		Integer temp = null;
+
+		
+		for (int i = 0; i < A.length; i++) {
+
+			
+			for (int j = 0; j < A.length; j++) {
+
+				if (i == 0) {
+					if (j == 0) {
+						temp = A[i][j];
+					}
+					if (j < N - 1) {
+						A[i][j] = A[i][j + 1];
+					}
+				}
+				else if (i > 0 && i < M - 1) {
+
+					if (j == 0) {						
+							int temp2 = A[i][0];
+							A[i][0] = temp;
+							temp = temp2;
+					}
+					if (j == N - 1) {
+						A[i - 1][N - 1] = A[i][N - 1];
+					}
+
+				}
+
+				else if (i == M - 1) {
+
+					if (j < N - 1) {
+						int temp2 = A[i][j];
+						A[i][j] = temp;
+						temp = temp2;
+					}
+					else if (j == N - 1) {
+						int temp2 = A[i][j];
+						A[i][j] = temp;
+						A[i - 1][j] = temp2;
+					}
+
+				}
+
+			}
 
 		}
-
-
 
 
 		AlgoMatrixRotation.printIntMatrix(A);
 
-
-
 		in.close();
-	}
 
+	}
 
 	/**
 	 * @param m
 	 */
-	public static void printIntMatrix(int[][] m){
+	public static void printIntMatrix(int[][] m) {
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
-				System.out.print(m[i][j]+" ");
+				System.out.print(m[i][j] + "\t");
 			}
 			System.out.println();
 		}
 	}
-
 
 }

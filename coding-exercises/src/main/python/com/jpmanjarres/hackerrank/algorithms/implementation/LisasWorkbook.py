3242cb2e347951bf5cd82f4fe28c06be3bcded0e
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2016-06-07 14:17:12 -0500

    Lisas Workbook, still incomplete

--- coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/LisasWorkbook.py
+++ coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/LisasWorkbook.py
@@ -1,11 +1,5 @@
 N,k = map(int, input().strip().split(" "))
-
 t = list(map(int, input().strip().split(" ")))
-
-
-#print(type(N))
-#print(type(k))
-#print(t)
 
 page = 1
 specials = 0
@@ -13,27 +7,36 @@ specials = 0
 for i in range(N):
     problem_number = 1
 
-    #print(" t[i]/k", t[i]//k)
-    #print(" t[i]%k", t[i]%k)
+    #print(" t[i]/k: ", t[i]//k," t[i]%k: ", t[i]%k)
 
     pages = 0
+
     if t[i] <= k :
         pages = 1
     else:
         pages = (t[i] // k ) + (t[i] % k)
 
-    print("Chapter %d Pages: %d" % (i+1,pages))
+    #print("Chapter %d Pages: %d" % (i+1,pages))
 
     for j in range(pages):
 
-        print("Page ",page," - ",(k*j+1),",",(k*j+k))
+        problems_per_page = 0
+        if t[i] >= k*(j+1):
+            problems_per_page = k
+        else:
+            problems_per_page = t[i] % k
 
-        if page >= k*j+1 and page <= k*j+k:
+        #print("Page ",page," - ",(k*j+1),",",(k*j+k)," - t[i] ",t[i], " Problems_page: ",problems_per_page)
+
+        if t[i] > k and page >= k*j+1 and page <= k*j+problems_per_page:
+            specials+=1
+        elif t[i] <= k and page >= 1 and page <= t[i]:
             specials+=1
 
         page+=1
 
-    print("Specials: ",specials)
+
+print(specials)
 
 
 

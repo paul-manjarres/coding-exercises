c090082709ecff01623133afc1a4d03e877fde94
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2016-10-03 23:53:59 -0500

    Circular Array Rotation - Accepted in Java, python is not fast enough.

---
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/warmup/CircularArrayRotationEasy.java
@@ -0,0 +1,106 @@
+package com.jpmanjarres.hackerrank.algorithms.warmup;
+
+import java.util.Scanner;
+
+/**
+ * @author <a href="paul.manjarres@gmail.com">Jean Paul Manjarres Correal</a> 03/10/2016
+ *
+ */
+public class CircularArrayRotationEasy {
+
+	/**
+	 * @param A
+	 * @return
+	 */
+	public static int[] rotateLeft(int[] A){
+		int l = A.length;
+		int first = A[0];
+		for (int j = 0; j < A.length-1; j++) {
+			A[j] = A[j+1];
+		}
+		A[l-1] = first;
+		return A;
+	}
+
+
+	/**
+	 * @param A
+	 * @return
+	 */
+	public static int[] rotateRight(int[] A){
+		int l = A.length;
+		int last = A[l-1];
+		int temp = A[0];
+
+		for (int j = 1; j < A.length; j++) {
+			int temp2 = A[j];
+			A[j] = temp;
+			temp = temp2;
+		}
+		A[0] = last;
+		return A;
+	}
+
+
+	/**
+	 * @param A
+	 * @param k
+	 * @return
+	 */
+	public static int[] rotate(int[] A, int k){
+
+		if(k<=0){
+			return A;
+		}
+
+		int l = A.length;
+	    int effective_rotations = k;
+	    boolean right = true;
+
+	    if( k > l){
+	        effective_rotations = k % l;
+	    }
+	    if( k > l/2){
+	        right = false;
+	        effective_rotations = l - effective_rotations;
+	    }
+
+	    for (int i = 0; i < effective_rotations; i++) {
+	    	if(right){
+	    		rotateRight(A);
+	    	}else{
+	    		rotateLeft(A);
+	    	}
+		}
+
+	    return A;
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		int K = in.nextInt();
+		int Q = in.nextInt();
+
+		int[] A = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			A[i] = in.nextInt();
+		}
+
+		A = rotate(A, K);
+
+		for (int i = 0; i < Q; i++) {
+			int index = in.nextInt();
+			System.out.println(A[index]);
+		}
+
+		in.close();
+	}
+
+}
--- coding-exercises/src/main/python/com/jpmanjarres/hackerrank/warmup/CircularArrayRotationEasy.py
+++ coding-exercises/src/main/python/com/jpmanjarres/hackerrank/warmup/CircularArrayRotationEasy.py
@@ -1,26 +1,62 @@
 
+# Algorithm is correct, but Python is not fast enough
+
 '''Rotates the array'''
 def rotate(a, k):
+
     if k<=0:
         return a
 
     l = len(a)
+
     effective_rotations = k
+    right = True
+
+    #print("k: %s  len: %s" % (k,l))
 
     if k > l :
-        effective_rotations = l % k
+        effective_rotations = k % l
+
+    if k > l/2:
+        right = False
+        effective_rotations = l - effective_rotations
+
+    #print("Effective rotations: %s" %effective_rotations)
+    #print("right: %s" % right)
 
     for j in range(effective_rotations):
+        if right:
+            rotate_right(a)
+        else:
+            rotate_left(a)
+        #print(a)
+    return a
 
-        last = a[l-1]
-        temp = a[0]
+'''Rotate once to right'''
+def rotate_right(a):
 
-        for i in range(1, l):
-            temp2 = a[i]
-            a[i] = temp
-            temp = temp2
+    l = len(a)
+    last = a[l-1]
+    temp = a[0]
 
-        a[0] = last
+    for i in range(1, l):
+        temp2 = a[i]
+        a[i] = temp
+        temp = temp2
+
+    a[0] = last
+
+    return a
+
+''' Rotate once to left'''
+def rotate_left(a):
+    l = len(a)
+    first = a[0]
+
+    for i in range(l-1):
+        a[i] = a[i+1]
+
+    a[l-1] = first
 
     return a
 

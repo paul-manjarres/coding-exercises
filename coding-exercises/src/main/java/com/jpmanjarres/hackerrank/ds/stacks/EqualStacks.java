9695d1564f6eb3b70296de0539392a9c56593195
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-03-16 17:10:29 -0500

    Equals Stacks fixed

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/ds/stacks/EqualStacks.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/ds/stacks/EqualStacks.java
@@ -24,97 +24,51 @@ public class EqualStacks {
 		int n2 = in.nextInt();
 		int n3 = in.nextInt();
 
-		final Deque<CustomStackNode> stack1 = new ArrayDeque<>(n1);
-		final Deque<CustomStackNode> stack2 = new ArrayDeque<>(n2);
-		final Deque<CustomStackNode> stack3 = new ArrayDeque<>(n3);
+		final Deque<Integer> stack1 = new ArrayDeque<>(n1 + 1);
+		final Deque<Integer> stack2 = new ArrayDeque<>(n2 + 1);
+		final Deque<Integer> stack3 = new ArrayDeque<>(n3 + 1);
 
-		int[] n1data = new int[n1];
-		int[] n2data = new int[n2];
-		int[] n3data = new int[n3];
+		int sum1 = 0;
+		int sum2 = 0;
+		int sum3 = 0;
+
+		int val =0;
 
 		for (int x = 0; x < n1; x++) {
-			n1data[x] = in.nextInt();
+			val = in.nextInt();
+			stack1.add(val);
+			sum1 += val;
 		}
 		for (int x = 0; x < n2; x++) {
-			n2data[x] = in.nextInt();
+			val = in.nextInt();
+			stack2.add(val);
+			sum2 += val;
 		}
 		for (int x = 0; x < n3; x++) {
-			n3data[x] = in.nextInt();
+			val = in.nextInt();
+			stack3.add(val);
+			sum3 += val;
 		}
 
-		for (int x = n1 - 1; x >= 0; x--) {
-			final CustomStackNode node = new CustomStackNode(n1data[x], n1data[x]);
-			if (!stack1.isEmpty()) {
-				node.currentMax = n1data[x] + stack1.peek().currentMax;
-			}
-			stack1.push(node);
-		}
-
-		for (int x = n2 - 1; x >= 0; x--) {
-			final CustomStackNode node = new CustomStackNode(n2data[x], n2data[x]);
-			if (!stack2.isEmpty()) {
-				node.currentMax = n2data[x] + stack2.peek().currentMax;
-			}
-			stack2.push(node);
-		}
-
-		for (int x = n3 - 1; x >= 0; x--) {
-			final CustomStackNode node = new CustomStackNode(n3data[x], n3data[x]);
-			if (!stack3.isEmpty()) {
-				node.currentMax = n3data[x] + stack3.peek().currentMax;
-			}
-			stack3.push(node);
-		}
-
-		n1data = null;
-		n2data = null;
-		n3data= null;
-
-		int maxValue = Integer.MAX_VALUE;
-
-		while(maxValue > 0){
-
-			int m1 = stack1.isEmpty()? 0 : stack1.peek().currentMax;
-			int m2 = stack2.isEmpty()? 0 : stack2.peek().currentMax;
-			int m3 = stack3.isEmpty()? 0 : stack3.peek().currentMax;
-
-			if(m1 == m2 && m1 == m3){
-				System.out.println(m1);
+		while (true) {
+			if (sum1 == sum2 && sum1 == sum3) {
+				System.out.println(sum1);
 				break;
 			}
 
-			if(m1 > m2 && m1 > m3){
-				stack1.pop();
-			}else if(m2 > m1 && m2 > m3){
-				stack2.pop();
-			}else{
-				stack3.pop();
+			if (sum1 >= sum2 && sum1 >= sum3) {
+				val = stack1.pop();
+				sum1 -= val;
+			} else if (sum2 >= sum1 && sum2 >= sum3) {
+				val = stack2.pop();
+				sum2 -= val;
+			} else if (sum3 >= sum1 && sum3 >= sum2) {
+				val = stack3.pop();
+				sum3 -= val;
 			}
 		}
 
-
-
 		in.close();
-	}
-
-	/////////////////////////////////////////////
-
-	static class CustomStackNode {
-		int val;
-		int currentMax;
-
-		public CustomStackNode() {
-		}
-
-		public CustomStackNode(int val, int currentMax) {
-			this.val = val;
-			this.currentMax = currentMax;
-		}
-
-		@Override
-		public String toString() {
-			return "[" + val + ", currMax=" + currentMax + "]";
-		}
 	}
 
 }

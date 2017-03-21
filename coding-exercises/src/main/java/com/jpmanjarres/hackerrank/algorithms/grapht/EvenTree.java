78762ee4f83116f2d36623db37b17852deda9ff6
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-03-21 17:55:09 -0500

    Added

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/grapht/EvenTree.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/grapht/EvenTree.java
@@ -6,7 +6,9 @@
  */
 package com.jpmanjarres.hackerrank.algorithms.grapht;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,7 +25,6 @@ public class EvenTree {
 		int N = in.nextInt();
 		int M = in.nextInt();
 
-		//List<List<Integer>> list = new ArrayList<>(N);
 
 		List<Integer>[] list = new List[N];
 		for (int i = 0; i < N; i++) {
@@ -32,12 +33,37 @@ public class EvenTree {
 		for (int i = 0; i < M; i++) {
 			int u = in.nextInt()-1;
 			int v = in.nextInt()-1;
-			list[u].add(v);
+			//list[u].add(v);
+			list[v].add(u);
 		}
 
+		int count =0;
+		Deque<Integer> stack = new ArrayDeque<>();
+		stack.add(0);
 
+		while(!stack.isEmpty()){
+			int root = stack.poll();
+			for(Integer i :  list[root]){
+				if(countNodes(i, list) % 2 ==0){
+					count++;
+				}
+				stack.add(i);
+			}
+		}
 
+		System.out.println(count);
 		in.close();
 	}
+
+
+
+	static int countNodes(int root, List<Integer>[] list){
+		int count = 1;
+		for(Integer i : list[root]){
+			count += countNodes(i, list);
+		}
+		return count;
+	}
+
 
 }
---
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/warmup/MiniMaxSum.java
@@ -0,0 +1,36 @@
+/**
+ * Evoki.co - Plume Works Copyright (c) 2013 - 2017 by Jean Paul Manjarres Correal - All Rights
+ * Reserved. Proprietary and Confidential - This source code is not for redistribution
+ * http://evoki.co Date: 21/03/2017
+ *
+ */
+package com.jpmanjarres.hackerrank.algorithms.warmup;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author <a href="mailto:paul.manjarres@gmail.com">Jean Paul Manjarres Correal. </a><br/>
+ * 21/03/2017
+ *
+ */
+public class MiniMaxSum {
+
+	public static void main(String[] args) {
+
+		final Scanner in = new Scanner(System.in);
+
+		int N = 5;
+		int[] d = new int[N];
+		for (int i = 0; i < N; i++) {
+			d[i] = in.nextInt();
+		}
+
+		Arrays.sort(d);
+		System.out.println(d[0]+d[1]+d[2]+d[3]+ " "+(d[1]+d[2]+d[3]+d[4]));
+
+		in.close();
+
+	}
+
+}

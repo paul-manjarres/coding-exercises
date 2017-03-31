26a74159a9b7b9c8bd45e5a8363e104254325139
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-03-30 23:08:31 -0500

    Added utility method to check prime numbers

--- coding-exercises/src/main/java/com/jpmanjarres/YagamiPaulUtils.java
+++ coding-exercises/src/main/java/com/jpmanjarres/YagamiPaulUtils.java
@@ -69,4 +69,17 @@ public class YagamiPaulUtils {
 		return arr;
 	}
 
+
+	public static boolean isPrime(int n) {
+		if (n < 2)
+			return false;
+		int root = (int) Math.sqrt(n);
+		for (int i = 2; i <= root; i++) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+
 }

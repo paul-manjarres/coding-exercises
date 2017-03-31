a43577f1d2630c7872a10c250f71bb27f8493f5d
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-03-30 21:03:51 -0500

    SeparateNumbers still need to fix a test case

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/SeparateTheNumbers.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/strings/SeparateTheNumbers.java
@@ -20,63 +20,67 @@ public class SeparateTheNumbers {
 
 		int q = in.nextInt();
 		for (int _i = 0; _i < q; _i++) {
-
 			String s = in.next();
 
-			boolean beau = true;
+			if (s.length() == 1) {
+				System.out.println("NO");
+			}
 
-			for (int i = 1; i <= s.length() / 2; i++) {
-				beau = true;
+			int size = 1;
+			int len = s.length();
+			char[] ar = s.toCharArray();
 
-				int[] arr = getIntArray(s.toCharArray(), i);
+			boolean btf = true;
 
-				for (int j = 0; j < arr.length - 1; j++) {
-					if (arr[j] >= arr[j + 1] || arr[j + 1] - arr[j] != 1) {
-						beau = false;
+			while (size <= len / 2) {
+
+				btf = true;
+				int start = 0;
+				long first = 0;
+
+				while (start + size <= len - size) {
+					long n = getNumber(ar, size, start);
+					long n2 = getNumber(ar, size, start + size);
+
+					if (start == 0) {
+						first = n;
+					}
+
+					long last = (long) Math.pow(10, size) - 1;
+					if (n == last) {
+						n2 = getNumber(ar, size + 1, start + size);
+					}
+
+					if ((n2 - n) != 1 || ar[start] == '0' || ar[start+size] == '0') {
+						btf = false;
 						break;
+					}
+					start += size;
+					if (n == last) {
+						size++;
 					}
 				}
 
-				if (beau) {
-					System.out.println("YES " + arr[0]);
+				if (btf) {
+					System.out.println("YES " + first);
 					break;
-
 				}
 
-			}
+				size++;
 
-			if (!beau) {
+			} // End While
+
+			if (!btf) {
 				System.out.println("NO");
-				break;
-
 			}
-
-
-
-
-			// for (int i = 0; i < arr.length; i++) {
-			// System.out.println(arr[i]+" ");
-			// }
 
 		}
 		in.close();
 	}
 
-	static int[] getIntArray(char[] arr, int size) {
-		int totalSize = (arr.length % size == 0) ? arr.length / size : (arr.length / size) + 1;
-		int[] ret = new int[totalSize];
-		int count = 0;
-
-		for (int i = 0; i < arr.length; i = i + size) {
-			char[] tmp = new char[size];
-			Arrays.fill(tmp, ' ');
-			for (int j = 0; j < size && j + i < arr.length; j++) {
-				tmp[j] = arr[i + j];
-			}
-			ret[count] = Integer.valueOf(new String(tmp).trim());
-			count++;
-		}
-		return ret;
+	static long getNumber(char[] ar, int size, int start) {
+		char[] tmp = Arrays.copyOfRange(ar, start, start + size);
+		return Long.valueOf(new String(tmp).trim());
 	}
 
 }

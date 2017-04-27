91bda1bce82e3841e4f14fc678a20d48d85e0246
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-04-27 16:36:39 -0500

    Grading students added

---
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/implementation/GradingStudents.java
@@ -0,0 +1,34 @@
+package com.jpmanjarres.hackerrank.algorithms.implementation;
+
+import java.util.Scanner;
+
+public class GradingStudents {
+
+
+	static int[] solve(int[] grades){
+		for(int i=0; i<grades.length; i++){
+			if(grades[i] >= 38){
+				int diff =  (5-grades[i]%5);
+
+				if(diff < 3){
+					grades[i] = grades[i] +  diff;
+				}
+			}
+		}
+		return grades;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] grades = new int[n];
+        for(int grades_i=0; grades_i < n; grades_i++){
+            grades[grades_i] = in.nextInt();
+        }
+        int[] result = solve(grades);
+        for(int grades_i=0; grades_i < n; grades_i++){
+            System.out.println(result[grades_i]);
+        }
+        in.close();
+    }
+}

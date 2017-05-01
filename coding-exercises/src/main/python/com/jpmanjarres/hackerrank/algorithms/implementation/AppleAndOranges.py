dcbb1b5e9c7658b2b69d3936b3070baaafd247b0
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-04-30 19:53:31 -0500

    Apples and oranges - Accepted

--- coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/AppleAndOranges.py
+++ coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/AppleAndOranges.py
@@ -6,3 +6,23 @@ m, n = input().strip().split(' ')
 m, n = [int(m), int(n)]
 apple = [int(apple_temp) for apple_temp in input().strip().split(' ')]
 orange = [int(orange_temp) for orange_temp in input().strip().split(' ')]
+
+count_apples = 0
+count_oranges = 0
+
+# Check apples
+for i in range(m):
+    d = a + apple[i]
+
+    if s <= d <= t:
+        count_apples = count_apples + 1
+
+# Check oranges
+for i in range(n):
+    d = b + orange[i]
+
+    if s <= d <= t:
+        count_oranges = count_oranges + 1
+
+print(count_apples)
+print(count_oranges)

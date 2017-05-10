2b2995338037b547a348175b4363dec7165b5e61
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-05-10 13:59:49 -0500

    Climbing the leaderboard, accepted by binary search

--- coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/ClimbingTheLeaderBoard.py
+++ coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/ClimbingTheLeaderBoard.py
@@ -1,28 +1,32 @@
 
-def main():
 
+def binary_search(a, x):
+    lo = 0
+    hi = len(a)
+
+    while lo < hi:
+        mid = (lo+hi)//2
+        midval = a[mid]
+        if midval > x:
+            lo = mid+1
+        elif midval < x:
+            hi = mid
+        else:
+            return mid
+    return lo
+
+
+def main():
     n = int(input().strip())
     scores = [int(a) for a in input().strip().split()]
     m = int(input().strip())
     alice = [int(a) for a in input().strip().split()]
-
+    # My code
 
     scores = sorted(list(set(scores)), reverse=True)
 
     for a in alice:
-
-        rank = 1
-        found = False
-        for i in range(len(scores)):
-            if a >= scores[i]:
-                print(rank)
-                found = True
-                break;
-            else:
-                rank = rank + 1
-
-        if not found:
-            print(rank)
-
+        index = binary_search(scores, a)
+        print(index+1)
 
 main()

b2f2b7a1a31b74e38b52dbb9f07abb9c7b7ee4df
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-05-15 16:36:25 -0500

    Jumping on the clouds - Accepted

--- coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/JumpingOnTheClouds.py
+++ coding-exercises/src/main/python/com/jpmanjarres/hackerrank/algorithms/implementation/JumpingOnTheClouds.py
@@ -1,18 +1,19 @@
-
 n = int(input().strip())
 c = [int(c_temp) for c_temp in input().strip().split(' ')]
 
 steps = 0
 curr = 0
 
-while curr < n-1:
+while curr < n - 1:
 
-    if c[curr+2] == 1:
-        steps = steps + 1
-        curr = curr + 1
+    if curr + 2 <= n - 1:
+        if c[curr + 2] == 0:
+            curr = curr + 2
+        else:
+            curr = curr + 1
     else:
-        steps = steps + 1
-        curr = curr + 2
+        curr = curr + 1
+
+    steps = steps + 1
 
 print(steps)
-

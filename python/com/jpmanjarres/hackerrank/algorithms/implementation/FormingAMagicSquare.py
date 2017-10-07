04bc143e898c69e6cf632313f7456fbe3ca0a9c8
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-10-07 17:56:19 -0500

    Moved python exercises to a separated projects.

---
+++ python/com/jpmanjarres/hackerrank/algorithms/implementation/FormingAMagicSquare.py
@@ -0,0 +1,15 @@
+s = []
+for s_i in range(3):
+    s_t = [int(s_temp) for s_temp in input().strip().split(' ')]
+    s.append(s_t)
+# Print the minimum cost of converting 's' into a magic square
+
+vals = []
+
+for i in range(3):
+    s[0][i]
+
+
+
+
+print(s)

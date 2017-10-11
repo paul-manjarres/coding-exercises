639df236e7505eac3f43759ff3135058a1c0e49b
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-10-11 17:08:50 -0500

    Maximum perimeter triangle, not implemented.

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/greedy/MaximumPerimeterTriangle.kt
@@ -0,0 +1,23 @@
+package org.yagamipaul.hackerrank.algorithms.greedy
+
+import java.util.*
+
+/**
+ * Maximum perimeter triangle
+ * Jean Paul Manjarres Correal
+ */
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val l = IntArray(n)
+    for (i in 0.until(n)) {
+        l[i] = scanner.nextInt()
+    }
+
+
+
+    scanner.close()
+
+}

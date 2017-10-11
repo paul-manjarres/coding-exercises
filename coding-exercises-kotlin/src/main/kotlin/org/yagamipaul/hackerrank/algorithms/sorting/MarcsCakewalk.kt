e7a2dc98f2aa6697a587090e16f2b8c0b167ed06
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-10-10 22:35:59 -0500

    marcs cakewalk added

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/sorting/MarcsCakewalk.kt
@@ -0,0 +1,23 @@
+package org.yagamipaul.hackerrank.algorithms.sorting
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val arr = IntArray(n)
+    for (i in 0..n - 1) {
+        arr[i] = scanner.nextInt()
+    }
+
+    var miles = 0L
+    Arrays.sort(arr)
+    arr.reverse()
+
+    for (i in 0.until(n)){
+        miles += arr[i]*Math.pow(2.0,i.toDouble()).toLong()
+    }
+    println(miles)
+
+}

9ed67a92e56799af80a485205401252761462ce4
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-10-12 17:47:30 -0500

    Greedy florist - not implemented yet

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/greedy/GreedyFlorist.kt
@@ -0,0 +1,21 @@
+package org.yagamipaul.hackerrank.algorithms.greedy
+
+import java.util.*
+
+/**
+ * Greedy Florist
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val k = scanner.nextInt()
+
+    val c = IntArray(n)
+    for (i in 0.until(n)) {
+        c[i] = scanner.nextInt()
+    }
+
+}
+

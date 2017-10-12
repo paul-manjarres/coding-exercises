28f15de3e7a56287bf093ed76604343f0db9cff5
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-10-12 14:01:02 -0500

    Greedy - MaxMin - Accepted

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/greedy/MaxMin.kt
@@ -0,0 +1,33 @@
+package org.yagamipaul.hackerrank.algorithms.greedy
+
+import java.util.*
+
+
+/**
+ * Max Min
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val k = scanner.nextInt()
+
+    val x = IntArray(n)
+    for (i in 0.until(n)) {
+        x[i] = scanner.nextInt()
+    }
+
+    Arrays.sort(x)
+//    println(x.contentToString())
+
+    var min = Int.MAX_VALUE
+
+    for( i in 0.until(n-k+1)){
+//        println("${x[i]} - ${x[i+k-1]}")
+        min = Math.min(min, x[i+k-1] - x[i])
+    }
+
+    println(min)
+    scanner.close()
+}

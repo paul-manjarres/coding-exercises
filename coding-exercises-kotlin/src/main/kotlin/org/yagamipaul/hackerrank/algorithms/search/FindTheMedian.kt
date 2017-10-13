fb75d62b915308141e4d6614154906ca3cdde189
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-10-12 22:49:55 -0500

    Find the median - Accepted

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/search/FindTheMedian.kt
@@ -0,0 +1,21 @@
+package org.yagamipaul.hackerrank.algorithms.search
+
+import java.util.*
+
+/**
+ * Find the median
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val c = IntArray(n)
+
+    for (i in 0.until(n)) {
+        c[i] = scanner.nextInt()
+    }
+
+    Arrays.sort(c)
+    println(c[n/2])
+}

5df2175862f9bcb992c6d1afe96e6545594d3aff
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-10-16 18:22:51 -0500

    Counting sort 1,2,3 accepted

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/sorting/CountingSort2.kt
@@ -0,0 +1,28 @@
+package org.yagamipaul.hackerrank.algorithms.sorting
+
+import java.util.*
+
+/**
+ * Counting Sort 2
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val arr = IntArray(100)
+    for (i in 0 until n) {
+        val number = scanner.nextInt()
+        arr[number]++
+    }
+
+
+    for(i in 0 until arr.size){
+
+        if(arr[i] > 0){
+            repeat(arr[i], {
+                print("$i ")
+            })
+        }
+    }
+
+}
---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/sorting/CountingSort3.kt
@@ -0,0 +1,28 @@
+package org.yagamipaul.hackerrank.algorithms.sorting
+
+import java.util.*
+
+
+/**
+ * Counting Sort 3
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val arr = IntArray(100)
+    for (i in 0 until n) {
+        val number = scanner.nextInt()
+        var str = scanner.nextLine()
+        arr[number]++
+    }
+
+
+    for(i in 0 until arr.size-1){
+        print("${arr[i]} ")
+        arr[i+1]+=arr[i]
+    }
+
+    print("${arr[99]}")
+
+}

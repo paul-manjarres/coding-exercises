882f68a69964f3941ff8f1bc2f7a086359ecf6da
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-10-24 13:59:25 -0500

    Sherlock and array, base class, not working. Kotlin

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/search/SherlockAndArray.kt
@@ -0,0 +1,28 @@
+package org.yagamipaul.hackerrank.algorithms.search
+
+import java.util.*
+
+
+/**
+ * Sherlock and Array
+ * https://www.hackerrank.com/challenges/sherlock-and-array
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val t = scanner.nextInt()
+
+    for (_i in 0  until t) {
+
+        val n = scanner.nextInt()
+        val a = IntArray(n)
+        for (i in 0  until n) {
+            a[i] = scanner.nextInt()
+        }
+
+
+
+    }
+    scanner.close()
+}

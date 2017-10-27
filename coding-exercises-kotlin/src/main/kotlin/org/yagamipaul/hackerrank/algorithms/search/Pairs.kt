6f1e56a6b48ffa50116d6bee9bc68d7e7e5f3455
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-10-27 14:21:57 -0500

    Pairs accepted. Kotlin

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/search/Pairs.kt
@@ -0,0 +1,28 @@
+package org.yagamipaul.hackerrank.algorithms.search
+
+import java.util.*
+
+
+/**
+ * Pairs
+ * https://www.hackerrank.com/challenges/pairs
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val k = scanner.nextInt()
+    var set = mutableSetOf<Long>()
+
+    for (i in 0.until(n)) {
+        set.add(scanner.nextLong())
+    }
+
+    var sum = 0
+    for(e in set){
+        if(set.contains(e+k)){
+            sum++
+        }
+    }
+    println(sum)
+}

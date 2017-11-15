42bde638116b8bc4e4f2ab8800fee6a888b3d735
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-11-14 22:38:55 -0500

    Missing numbers accepted. Kotlin

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/search/MissingNumbers.kt
@@ -0,0 +1,60 @@
+package org.yagamipaul.hackerrank.algorithms.search
+
+import java.util.*
+import kotlin.collections.HashSet
+
+
+/**
+ * Missing numbers
+ * https://www.hackerrank.com/challenges/minimum-loss
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    val n = scanner.nextInt()
+    val a = IntArray(n)
+    var freqA = HashMap<Int, Int>()
+    for ( i in 0 until n){
+        a[i] = scanner.nextInt()
+        var count = freqA.getOrDefault(a[i], 0)
+        freqA[a[i]] = count+1
+    }
+
+    val m = scanner.nextInt()
+    val b = IntArray(m)
+    var freqB = HashMap<Int, Int>()
+    for ( i in 0 until m){
+        b[i] = scanner.nextInt()
+        var count = freqB.getOrDefault(b[i], 0)
+        freqB[b[i]] = count+1
+    }
+
+    val missingTotal = m - n
+    var missing=0
+
+    val missingValues = TreeSet<Int>()
+
+    for( (key,value) in freqB){
+
+        val item = freqA.getOrDefault(key, -1)
+
+        if(item==-1){
+            missingValues.add(item)
+            missing+=(value)
+        }else if(item < value){
+            missingValues.add(key)
+            missing+= (value-item)
+        }
+        if(missing==missingTotal){
+            break
+        }
+    }
+
+    missingValues.forEach{
+        i-> print("$i ")
+    }
+
+
+
+}

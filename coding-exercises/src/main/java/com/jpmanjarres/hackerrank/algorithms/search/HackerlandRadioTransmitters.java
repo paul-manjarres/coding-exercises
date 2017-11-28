0de0ae5e29711e19db4a3c36004a4c4eff5e85a5
Jean Paul Manjarres Correal <manjarj@colpatria.com>
2017-11-28 17:03:20 -0500

    Hackerland radio transmitters, still not working

--- coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/search/HackerlandRadioTransmitters.java
+++ coding-exercises/src/main/java/com/jpmanjarres/hackerrank/algorithms/search/HackerlandRadioTransmitters.java
@@ -4,8 +4,7 @@ package com.jpmanjarres.hackerrank.algorithms.search;
 import java.util.*;
 
 /**
- * Hackerland Radio Transmitters
- * https://www.hackerrank.com/challenges/hackerland-radio-transmitters/problem
+ * Hackerland Radio Transmitters https://www.hackerrank.com/challenges/hackerland-radio-transmitters/problem
  * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
  */
 public class HackerlandRadioTransmitters {
@@ -16,68 +15,36 @@ public class HackerlandRadioTransmitters {
         final int n = in.nextInt();
         final int k = in.nextInt();
         final int[] x = new int[n];
-        final int k2 = k*2;
 
-
-        final Set<Integer> set = new HashSet<>();
+        final int k2 = k * 2;
 
         for (int i = 0; i < n; i++) {
             x[i] = in.nextInt();
-            set.add(x[i]);
         }
 
         Arrays.sort(x);
         int i = 0;
         int count = 0;
-        boolean covered = false;
 
-
-
-        while(i < n){
+        while (i < n) {
 
             int c = x[i];
-            if(i+1 == n){
-                count++;
-                break;
+            int curr = c;
+
+            while (curr < c + k && i < n - 1) {
+                curr = x[++i];
             }
 
-            int c2 = x[i+1];
-
-            if(c2-c > k){
-                count++;
-                i=i+1;
-            }else if (c2-c == k){
-                count++;
-                while(c2 < c+k2) {
-                    i++;
-                    if(i+1 == n){
-                        count++;
-                        break;
-                    }
-                    c2 = x[i];
-                }
-                i++;
-            }else{
-
-                while(c2 < c+k && i<n-2){
-                    i++;
-                    c2 = x[i+1];
-                }
-                count++;
-                c= x[i];
-
-                while(c2 < c+k && i<n-2) {
-                    i++;
-                    c2 = x[i];
-                }
-
+            count++;
+            if(curr > c+k){
+                continue;
             }
 
-
+            while (curr < c + k2 && i < n - 1) {
+                curr = x[++i];
+            }
+            i++;
         }
-
         System.out.println(count);
-
-
     }
 }

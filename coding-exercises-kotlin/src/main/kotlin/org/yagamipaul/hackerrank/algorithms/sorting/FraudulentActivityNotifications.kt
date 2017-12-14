f953717188e9eee97f6845074318dd08db5af927
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-12-13 20:42:46 -0500

    Added Fraudulent Activity notification, still not working.

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/sorting/FraudulentActivityNotifications.kt
@@ -0,0 +1,28 @@
+package org.yagamipaul.hackerrank.algorithms.sorting
+
+import java.util.*
+
+
+/**
+ * Fraudulent Activity Notifications
+ * https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem
+ * Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
+ */
+fun main(args: Array<String>) {
+
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val d = scanner.nextInt()
+
+    val arr = IntArray(n)
+
+    for(_i in 0 until n){
+        arr[_i] = scanner.nextInt()
+    }
+
+
+
+
+    scanner.close()
+
+}

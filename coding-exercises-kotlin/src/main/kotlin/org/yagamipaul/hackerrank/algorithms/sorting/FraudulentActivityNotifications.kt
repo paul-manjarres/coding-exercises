f6b95bde87a4c6a63d65c829b14ff93514d55937
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-12-14 23:09:02 -0500

    Fraudulent Activity Notifications - Still timeout

--- coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/sorting/FraudulentActivityNotifications.kt
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/sorting/FraudulentActivityNotifications.kt
@@ -23,39 +23,48 @@ fun main(args: Array<String>) {
 }
 
 
+/**
+ * Solves the problem, returns the solution
+ */
 private fun solve(arr: IntArray, n: Int, d: Int): Int {
 
-    var notifications = 0
-
-    if(d==n){
+    // if the window size is equals the size of the array.
+    if (d == n) {
         return 0
     }
+
+    var notifications = 0
 
     val freq = IntArray(201)
     for (i in 0 until d) {
         freq[arr[i]]++
     }
 
-    for( i in d until n){
+    for (i in d until n) {
         var median = findMedian(freq, d)
-        if(arr[i] >= 2*median){
+        if (arr[i] >= 2 * median) {
             notifications++
         }
 
         freq[arr[i]]++
-        freq[arr[i-d]]--
+        freq[arr[i - d]]--
     }
 
     return notifications
 
 }
 
-private fun findMedian(freq: IntArray, d: Int): Int {
+private fun findMedian(freq: IntArray, d: Int): Double {
 
     val sortedArray = IntArray(d)
     var j = 0
 
     for (i in 0 until freq.size) {
+
+        if (j == d) {
+            break
+        }
+
         if (freq[i] > 0) {
             var k = 0
             while (k < freq[i]) {
@@ -67,7 +76,7 @@ private fun findMedian(freq: IntArray, d: Int): Int {
         }
     }
 
-    val median = if (d % 2 == 0) (sortedArray[d / 2] + sortedArray[d / 2 + 1]) / 2 else sortedArray[d / 2]
+    val median = if (d % 2 == 0) (sortedArray[d / 2 -1] + sortedArray[d / 2 ]) / 2.0 else sortedArray[d / 2].toDouble()
     //sortedArray.forEach { print("$it ") }
     //println("median = $median")
 

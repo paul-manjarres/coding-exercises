ea8376eebe99b10efc863097d941c8ae4cb145ff
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2018-10-13 21:06:04 -0500

    Append and delete accepted

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/implementation/AppendAndDelete.kt
@@ -0,0 +1,37 @@
+package org.yagamipaul.hackerrank.algorithms.implementation
+
+import java.util.*
+
+
+class AppendAndDelete {
+    // Complete the appendAndDelete function below.
+    fun appendAndDelete(s: String, t: String, k: Int): String {
+        var count = 0
+        for(i in 0.until(Math.min(s.length, t.length))){
+            if(s[i] == t[i]){
+                count++
+            }else{
+                break
+            }
+        }
+
+        //calculate the number of different chars in both strings
+        var diff = s.length + t.length - (2*count)
+
+        return if(diff>k) "No"
+            else if(count == 0 || diff == k) "Yes"
+            else if((k - diff) %2 == 0) "Yes"
+            else if(k > diff + 2*count) "Yes"
+            else "No"
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val s = scan.nextLine()
+    val t = scan.nextLine()
+    val k = scan.nextLine().trim().toInt()
+    val result = AppendAndDelete().appendAndDelete(s, t, k)
+    println(result)
+}
---
+++ coding-exercises-kotlin/src/test/kotlin/org/yagamipaul/hackerrank/algorithms/implementation/AppendAndDeleteTest.kt
@@ -0,0 +1,57 @@
+package org.yagamipaul.hackerrank.algorithms.implementation
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+class AppendAndDeleteTest {
+
+    @Test
+    fun `test case 1`() {
+        val res = AppendAndDelete().appendAndDelete("hackerhappy","hackerrank", 9)
+        Assertions.assertThat(res).isEqualTo("Yes")
+    }
+
+    @Test
+    fun `test case 2`() {
+        val res = AppendAndDelete().appendAndDelete("aba","aba", 7)
+        Assertions.assertThat(res).isEqualTo("Yes")
+    }
+
+    @Test
+    fun `test case 2a`() {
+        val res = AppendAndDelete().appendAndDelete("aaaaaaaaaa","aaaaa", 7)
+        Assertions.assertThat(res).isEqualTo("Yes")
+    }
+
+    @Test
+    fun `test case 3`() {
+        val res = AppendAndDelete().appendAndDelete("ashley","ash", 2)
+        Assertions.assertThat(res).isEqualTo("No")
+    }
+
+    @Test
+    fun `test case 5`() {
+        val res = AppendAndDelete().appendAndDelete("y","yu", 2)
+        Assertions.assertThat(res).isEqualTo("No")
+    }
+
+    @Test
+    fun `test case 9`() {
+        val res = AppendAndDelete().appendAndDelete("abcdef","fedcba", 15)
+        Assertions.assertThat(res).isEqualTo("Yes")
+    }
+
+    @Test
+    fun `test case 10`() {
+        val res = AppendAndDelete().appendAndDelete("abcd","abcdert", 10)
+        Assertions.assertThat(res).isEqualTo("No")
+    }
+
+    @Test
+    fun `test case 12`() {
+        val res = AppendAndDelete().appendAndDelete("aaa","a", 5)
+        Assertions.assertThat(res).isEqualTo("Yes")
+    }
+
+}

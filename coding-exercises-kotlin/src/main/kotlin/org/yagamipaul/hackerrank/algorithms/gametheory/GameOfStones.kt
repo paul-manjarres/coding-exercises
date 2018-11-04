eb609ded40d705f301794cdcb333ee43e53e8f2b
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2018-11-04 16:23:46 -0500

    Game of stones accepted

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/gametheory/GameOfStones.kt
@@ -0,0 +1,24 @@
+package org.yagamipaul.hackerrank.algorithms.gametheory
+
+import java.util.*
+
+class GameOfStones {
+
+    // Complete the gameOfStones function below.
+    fun gameOfStones(n: Int): String {
+        return if (n%7==0 || n%7==1) "Second" else "First"
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val t = scan.nextLine().trim().toInt()
+
+    for (tItr in 1..t) {
+        val n = scan.nextLine().trim().toInt()
+        val result = GameOfStones().gameOfStones(n)
+        println(result)
+    }
+}
---
+++ coding-exercises-kotlin/src/test/kotlin/org/yagamipaul/hackerrank/algorithms/gametheory/GameOfStonesTest.kt
@@ -0,0 +1,44 @@
+package org.yagamipaul.hackerrank.algorithms.gametheory
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GameOfStonesTest {
+
+    @Test
+    fun `test case 1`() {
+        val res = GameOfStones().gameOfStones(1)
+        Assertions.assertThat(res).isEqualTo("Second")
+    }
+
+    @Test
+    fun `test case 2`() {
+        val res = GameOfStones().gameOfStones(2)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 3`() {
+        val res = GameOfStones().gameOfStones(3)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 4`() {
+        val res = GameOfStones().gameOfStones(4)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 7`() {
+        val res = GameOfStones().gameOfStones(7)
+        Assertions.assertThat(res).isEqualTo("Second")
+    }
+
+    @Test
+    fun `test case 8`() {
+        val res = GameOfStones().gameOfStones(10)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+}

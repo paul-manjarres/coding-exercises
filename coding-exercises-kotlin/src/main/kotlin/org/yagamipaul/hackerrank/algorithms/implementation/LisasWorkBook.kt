4d45f3ac42cd152756f6678a67b3655c4a95757e
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2018-11-11 20:26:05 -0500

    Lisas Workbook accepted, kotlin

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/implementation/LisasWorkBook.kt
@@ -0,0 +1,52 @@
+package org.yagamipaul.hackerrank.algorithms.implementation
+
+import java.util.*
+
+class LisasWorkBook {
+
+    // Complete the workbook function below.
+    fun workbook(n: Int, k: Int, arr: Array<Int>): Int {
+
+        var page = 0
+        var special = 0
+
+        // Iterate over chapters, i = chapter
+        for(i in 0 until n){
+            page++
+            var pageInChapter = 1
+
+            // Iterate over problems inside chapter
+            for(j in 1 until arr[i]+1){
+                if(j/ k.toDouble() > pageInChapter ){
+                    pageInChapter++
+                    page++
+                }
+                if(j == page){
+                    special++
+                }
+            }
+        }
+        return special
+    }
+
+
+
+
+}
+
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nk = scan.nextLine().split(" ")
+
+    val n = nk[0].trim().toInt()
+
+    val k = nk[1].trim().toInt()
+
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = LisasWorkBook().workbook(n, k, arr)
+
+    println(result)
+}
---
+++ coding-exercises-kotlin/src/test/kotlin/org/yagamipaul/hackerrank/algorithms/implementation/LisasWorkBookTest.kt
@@ -0,0 +1,13 @@
+package org.yagamipaul.hackerrank.algorithms.implementation
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class LisasWorkBookTest {
+
+    @Test
+    fun `test case 1`() {
+        val res = LisasWorkBook().workbook(5,3, arrayOf(4,2,6,1,10))
+        Assertions.assertThat(res).isEqualTo(4)
+    }
+}

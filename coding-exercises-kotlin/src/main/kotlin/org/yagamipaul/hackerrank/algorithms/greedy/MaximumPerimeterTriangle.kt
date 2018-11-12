172ede5cee6316fda3cb64032971ddcc138ea64e
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2018-11-11 22:40:03 -0500

    Maximum perimeter triangle accepted, Kotlin

--- coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/greedy/MaximumPerimeterTriangle.kt
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/greedy/MaximumPerimeterTriangle.kt
@@ -6,18 +6,34 @@ import java.util.*
  * Maximum perimeter triangle
  * Jean Paul Manjarres Correal
  */
+class MaximumPerimeterTriangle{
+
+    // Complete the maximumPerimeterTriangle function below.
+    fun maximumPerimeterTriangle(sticks: Array<Int>): Array<String> {
+
+        var response = arrayOf("-1","","")
+
+        val sorted = sticks.sortedArray()
+        var i = sorted.size - 3
+
+        while (i>=0){
+            if( sorted[i] + sorted[i+1] > sorted[i+2]){
+                response[0] = sorted[i].toString()
+                response[1] = sorted[i+1].toString()
+                response[2] = sorted[i+2].toString()
+                return response
+            }
+            i--
+        }
+        return response
+    }
+}
 
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-
-    val n = scanner.nextInt()
-    val l = IntArray(n)
-    for (i in 0.until(n)) {
-        l[i] = scanner.nextInt()
-    }
-
-
-
-    scanner.close()
-
+    val scan = Scanner(System.`in`)
+    val n = scan.nextLine().trim().toInt()
+    val sticks = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    val result = MaximumPerimeterTriangle().maximumPerimeterTriangle(sticks)
+    println(result.joinToString(" "))
 }
+
---
+++ coding-exercises-kotlin/src/test/kotlin/org/yagamipaul/hackerrank/algorithms/greedy/MaximumPerimeterTriangleTest.kt
@@ -0,0 +1,23 @@
+package org.yagamipaul.hackerrank.algorithms.greedy
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class MaximumPerimeterTriangleTest {
+
+    @Test
+    fun `test case 1`() {
+        val res = MaximumPerimeterTriangle().maximumPerimeterTriangle(arrayOf(1,1,1,3,3))
+        Assertions.assertThat(Arrays.equals(res, arrayOf("1","3","3"))).isTrue()
+    }
+
+    @Test
+    fun `test case 2`() {
+        val res = MaximumPerimeterTriangle().maximumPerimeterTriangle(arrayOf(1,1,1,2,3,5))
+        Assertions.assertThat(Arrays.equals(res, arrayOf("1","1","1"))).isTrue()
+    }
+
+
+
+}

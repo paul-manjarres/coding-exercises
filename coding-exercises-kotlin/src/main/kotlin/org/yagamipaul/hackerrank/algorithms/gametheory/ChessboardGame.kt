8e33a1ff3bb1576105467b8918e92da6f4afd377
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2018-11-17 20:16:44 -0500

    Chessboard game accepted, Kotlin

---
+++ coding-exercises-kotlin/src/main/kotlin/org/yagamipaul/hackerrank/algorithms/gametheory/ChessboardGame.kt
@@ -0,0 +1,59 @@
+package org.yagamipaul.hackerrank.algorithms.gametheory
+
+import java.util.*
+
+class ChessboardGame {
+
+    // Complete the chessboardGame function below.
+    fun chessboardGame(x: Int, y: Int): String {
+        return if( f(x-1,y-1) == 1) "First" else "Second"
+    }
+
+    fun f(x:Int, y:Int): Int{
+
+        // Base cases
+        if(x==0 && (y==0 || y==1)){
+            return 0
+        }
+        if(x==1 && y==0){
+            return 0
+        }
+
+        if(x==0 && (y==2 || y==3)){
+            return 1
+        }
+
+        if(y==0 && (x==2 || x==3)){
+            return 1
+        }
+
+        if(x>=1 && y>=1){
+            var b1 = f(0,y) == 1
+            var b2 = f(x,0) == 1
+            return if( b1 || b2 ) return 1 else 0
+        }
+
+        if(x==0 && y>3){
+            return f(0,y-4)
+        }
+        if(y==0 && x>3){
+            return f(x-4,0)
+        }
+
+        return 0
+   }
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val t = scan.nextLine().trim().toInt()
+    for (tItr in 1..t) {
+        val xy = scan.nextLine().split(" ")
+        val x = xy[0].trim().toInt()
+        val y = xy[1].trim().toInt()
+        val result = ChessboardGame().chessboardGame(x, y)
+        println(result)
+    }
+}
---
+++ coding-exercises-kotlin/src/test/kotlin/org/yagamipaul/hackerrank/algorithms/gametheory/ChessboardGameTest.kt
@@ -0,0 +1,61 @@
+package org.yagamipaul.hackerrank.algorithms.gametheory
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ChessboardGameTest {
+
+    @Test
+    fun `test case 1`() {
+        val res = ChessboardGame().chessboardGame(5,2)
+        Assertions.assertThat(res).isEqualTo("Second")
+    }
+
+    @Test
+    fun `test case 2`() {
+        val res = ChessboardGame().chessboardGame(5,3)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 3`() {
+        val res = ChessboardGame().chessboardGame(8,8)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 4`() {
+        val res = ChessboardGame().chessboardGame(1,5)
+        Assertions.assertThat(res).isEqualTo("Second")
+    }
+
+    @Test
+    fun `test case 5`() {
+        val res = ChessboardGame().chessboardGame(1,6)
+        Assertions.assertThat(res).isEqualTo("Second")
+    }
+
+    @Test
+    fun `test case 6`() {
+        val res = ChessboardGame().chessboardGame(1,15)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 7`() {
+        val res = ChessboardGame().chessboardGame(4,15)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 8`() {
+        val res = ChessboardGame().chessboardGame(2,3)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+
+    @Test
+    fun `test case 9`() {
+        val res = ChessboardGame().chessboardGame(2,4)
+        Assertions.assertThat(res).isEqualTo("First")
+    }
+}

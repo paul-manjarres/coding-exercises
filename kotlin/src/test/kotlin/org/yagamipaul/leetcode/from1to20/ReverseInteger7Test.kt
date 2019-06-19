89c8c01f3d92b9c91bda9290dbc0a4acdc821bcc
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2019-06-18 21:31:55 -0500

    Added LeetCode #7 Reverse integer

---
+++ kotlin/src/test/kotlin/org/yagamipaul/leetcode/from1to20/ReverseInteger7Test.kt
@@ -0,0 +1,22 @@
+package org.yagamipaul.leetcode.from1to20
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ReverseInteger7Test {
+
+    @Test
+    fun `Test example`(){
+        var obj = ReverseInteger7()
+        Assertions.assertThat(obj.reverse(123)).isEqualTo(321)
+        Assertions.assertThat(obj.reverse(-123)).isEqualTo(-321)
+        Assertions.assertThat(obj.reverse(120)).isEqualTo(21)
+        Assertions.assertThat(obj.reverse(123)).isEqualTo(321)
+        Assertions.assertThat(obj.reverse(1534236469)).isEqualTo(0)
+        Assertions.assertThat(obj.reverse(-2147483648)).isEqualTo(0)
+
+
+
+    }
+
+}

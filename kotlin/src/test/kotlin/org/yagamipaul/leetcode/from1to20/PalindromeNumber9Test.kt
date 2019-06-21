a7292a17ff5d05898a4ea88b78d4391aeb849981
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2019-06-20 20:13:16 -0500

    Added Leetcode palindrome number 3

---
+++ kotlin/src/test/kotlin/org/yagamipaul/leetcode/from1to20/PalindromeNumber9Test.kt
@@ -0,0 +1,16 @@
+package org.yagamipaul.leetcode.from1to20
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PalindromeNumber9Test {
+
+    @Test
+    fun `Test cases`(){
+        var obj = PalindromeNumber9()
+        Assertions.assertThat(obj.isPalindrome(121)).isTrue()
+        Assertions.assertThat(obj.isPalindrome(-121)).isFalse()
+        Assertions.assertThat(obj.isPalindrome(10)).isFalse()
+        Assertions.assertThat(obj.isPalindrome(1345431)).isTrue()
+    }
+}

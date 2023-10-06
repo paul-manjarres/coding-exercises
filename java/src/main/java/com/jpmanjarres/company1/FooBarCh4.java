922fd246be9dde472d25fde58d5658877e9f313f
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2023-10-06 17:47:58 -0500

    Added changes to Foobar4 still not ready

--- java/src/main/java/com/jpmanjarres/company1/FooBarCh4.java
+++ java/src/main/java/com/jpmanjarres/company1/FooBarCh4.java
@@ -1,6 +1,5 @@
 package com.jpmanjarres.company1;
 
-import javax.sound.sampled.Line;
 import java.util.*;
 
 public class FooBarCh4 {
@@ -14,101 +13,160 @@ public class FooBarCh4 {
     final List<Integer> terminals = findTerminalIds(m);
     System.out.println("Terminals: " + terminals);
 
+    final int[] totalEvents = new int[m.length];
+    for (int i = 0; i < m.length; i++) {
+      for (int j = 0; j < m[i].length; j++) {
+        totalEvents[i] += m[i][j];
+      }
+    }
+
+    System.out.println(Arrays.toString(totalEvents));
+
+
     final int[] probabilities = new int[terminals.size() + 1];
 
     // 2. Identificar cuales son inalcanzables, P=0%
     final List<Integer> unreachables = findUnreachables(m);
     System.out.println("unreachables: " + unreachables);
 
-
     // 3. Identificar P del camino directo
-
     Queue<Integer> queue = new ArrayDeque<>();
     queue.add(0);
-    int[] visited = new int[m.length];
-    //visited[0] = 1;
 
-    List<List<Integer>> pathsTo5 = new ArrayList<>();
-    List<Integer> currentPath = new ArrayList<>();
-    currentPath.add(0);
-    boolean x = visit(m, 0, visited, 5, currentPath, pathsTo5);
+    for (Integer t : terminals) {
+      //int t=5;
+      int[] visited = new int[m.length];
+      List<PathProb> paths = new ArrayList<>();
+      PathProb currentPath = new PathProb();
+      currentPath.addNode(0, new Rational(1, 1));
+      visit(m, 0, visited, t, currentPath, paths, totalEvents);
+      paths.stream().forEach(p -> p.reduceProbabilities());
 
-    System.out.println("Paths to : "+pathsTo5);
+      Rational sum = new Rational(0, 1);
+      for (PathProb p : paths) {
+        sum = Rational.sum(sum, p.getProbabilities().get(0));
+      }
 
-    // 4 Identificar P de ciclos
-    //
-
+      System.out.println("Paths to : " + t + " : " + paths + " - Sum: " + sum.simplify());
+    }
     System.out.println("Probabilities: " + Arrays.toString(probabilities));
 
     return new int[]{7, 6, 8, 21};
   }
 
+  public static boolean visit(int[][] arr, int node, int[] visited, int target, PathProb currentPath, List<PathProb> paths, int[] totalEvents) {
 
-
-  public static boolean visit(int[][] arr, int node, int[] visited, int target, List<Integer> currentPath, List<List<Integer>> paths) {
-
-    System.out.println("Current Path: "+ currentPath);
-    visited[node] = visited[node]+1;
+    //System.out.println("Current Path: " + currentPath);
+    visited[node] = visited[node] + 1;
     if (node == target) {
-      paths.add(new ArrayList<>(currentPath));
-      System.out.println("Found Path!");
+      paths.add(PathProb.from(currentPath));
+      //System.out.println("Found Path! : " + currentPath);
       return true;
     }
 
-
     for (int i = 0; i < arr[node].length; i++) {
       int childProb = arr[node][i];
+      Rational probability = new Rational(childProb, totalEvents[node]);
       int child = i;
       if (childProb > 0 && (visited[child] == 0 || visited[child] == 1)) {
-        currentPath.add(child);
-        System.out.println("Adding "+child);
-        boolean correct = visit(arr, child, Arrays.copyOf(visited, visited.length), target, currentPath, paths);
-        //if(!correct){
-          System.out.println("Removing last "+currentPath.get(currentPath.size()-1));
-          currentPath.remove(currentPath.size()-1);
-
-        //}
-//        else{
-//          return true;
-//        }
+        currentPath.addNode(child, probability);
+        //System.out.println("Adding " + child + " Prob: " + probability);
+        visit(arr, child, Arrays.copyOf(visited, visited.length), target, currentPath, paths, totalEvents);
+        currentPath.removeLast();
       } else if (childProb > 0 && visited[child] > 1) {
         //Permite e identifica el ciclo.
-        System.out.println("Ciclo:: " + child+ " - "+currentPath.get(currentPath.size()-1));
+        Rational p1 = currentPath.getProbabilities().get(currentPath.probabilities.size()-3);
+        Rational p2 = currentPath.getProbabilities().get(currentPath.probabilities.size()-2);
+        Rational act = currentPath.probabilities.remove(currentPath.probabilities.size()-1);
+        Rational cicleMultiplier = new Rational(p1.d *p2.d, (p1.d *p2.d - p1.n *p2.n)).simplify();
+        currentPath.probabilities.remove(currentPath.probabilities.size()-1);
+
+        currentPath.probabilities.add(Rational.multiply(p2, cicleMultiplier));
+        currentPath.probabilities.add(act);
+        System.out.println("Ciclo:: " + child + " - " + currentPath+ " P1: "+p1+ " - P2: "+p2);
+
+      }
+    }
+    return false;
+  }
+
+  private static class PathProb {
+    private List<Integer> path = new ArrayList<>();
+    private List<Rational> probabilities = new ArrayList<>();
+
+    static PathProb from(PathProb p) {
+      PathProb pb = new PathProb();
+      pb.path = new ArrayList<>(p.path);
+      pb.probabilities = new ArrayList<>(p.probabilities);
+      return pb;
+    }
+
+    void addNode(Integer node, Rational probability) {
+      this.path.add(node);
+      if(!(probability.n == 1 && probability.d == 1)) {
+      this.probabilities.add(probability.simplify());
       }
     }
 
+    void removeLast() {
+      this.path.remove(this.path.size() - 1);
+      this.probabilities.remove(this.probabilities.size() - 1);
+    }
 
-    return false;
-}
+    List<Rational> getProbabilities() {
+      return probabilities;
+    }
 
+    void reduceProbabilities(){
+      int denominator = this.probabilities.stream().map(r-> r.d).reduce(1, (a, b)-> a*b);
+      int numerator = this.probabilities.stream().map(r-> r.n).reduce(1, (a, b)-> a*b);
+      this.probabilities.clear();
+      this.probabilities.add(new Rational(numerator, denominator).simplify());
+    }
 
-//  public static List<Integer> visit(int[][] arr, Queue<Integer> queue, boolean[] visited, int target, List<Integer> currentPath, List<List<Integer>> paths, List<Integer> terminals) {
-//
-//    int node = queue.poll();
-//
-//    if (terminals.contains(node)) {
-//      if (node == target) {
-//        currentPath.add(node);
-//        paths.add(currentPath);
-//        return currentPath;
-//      } else {
-//        return new LinkedList<>();
-//      }
-//    }
-//
-//    for (int i = 0; i < arr[node].length; i++) {
-//      int child = arr[node][i];
-//      if (child > 0 && !visited[child]) {
-//        queue.add(i);
-//        visited[i] = true;
-//
-//        currentPath.addAll(visit(arr, queue, visited, target, currentPath, paths, terminals));
-//      }
-//    }
-//
-//    return currentPath;
-//}
+    @Override
+    public String toString() {
+      return "{" +
+          "path=" + path +
+          ", probabilities=" + probabilities +
+          '}';
+    }
+  }
 
+  private static class Rational {
+    int n;
+    int d;
+
+    Rational(int a, int b) {
+      this.n = a;
+      this.d = b;
+    }
+
+    static Rational sum(Rational a, Rational b){
+      return new Rational(a.n*b.d+b.n*a.d, a.d*b.d);
+    }
+    static Rational multiply(Rational a, Rational b){
+      return new Rational(a.n*b.n, a.d*b.d);
+    }
+
+    Rational simplify(){
+      int bigger = Math.max(this.n, this.n);
+      Rational result = this;
+      for (int i = 2; i < bigger ; ) {
+        if(result.n %i == 0 && result.d%i == 0){
+          result = new Rational(result.n/i, result.d/i);
+        }else{
+          i++;
+        }
+      }
+      return result;
+    }
+
+    @Override
+    public String toString() {
+      return n + "/" + d;
+    }
+  }
 
   public static List<Integer> findTerminalIds(int[][] m) {
     List<Integer> terminals = new ArrayList<>();

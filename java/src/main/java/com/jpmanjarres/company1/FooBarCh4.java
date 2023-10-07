045e34a9bafc8d5789d253d45e387ea1ac2384f3
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2023-10-06 19:53:35 -0500

    Refactor

--- java/src/main/java/com/jpmanjarres/company1/FooBarCh4.java
+++ java/src/main/java/com/jpmanjarres/company1/FooBarCh4.java
@@ -6,12 +6,7 @@ public class FooBarCh4 {
 
   public static int[] solution(int[][] m) {
     // Your code here
-    printIntMatrix(m);
-
-    // 0. TODO Identificar los casos bordes, triviales
-    // 1. Identificar cuales son terminales
-    final List<Integer> terminals = findTerminalIds(m);
-    System.out.println("Terminals: " + terminals);
+    //printIntMatrix(m);
 
     final int[] totalEvents = new int[m.length];
     for (int i = 0; i < m.length; i++) {
@@ -19,99 +14,78 @@ public class FooBarCh4 {
         totalEvents[i] += m[i][j];
       }
     }
-
-    System.out.println(Arrays.toString(totalEvents));
-
-
-    final int[] probabilities = new int[terminals.size() + 1];
-
-    // 2. Identificar cuales son inalcanzables, P=0%
-    //final List<Integer> unreachables = findUnreachables(m);
-    //System.out.println("unreachables: " + unreachables);
-
-    // 3. Identificar P del camino directo
-
     int lcm = 1;
+    final List<Rational> terminalProbabilities = new ArrayList<>();
+    final List<Integer> terminals = findTerminalIds(m);
 
-    List<Rational> terminalProbabilities = new ArrayList<>();
     for (Integer t : terminals) {
-
       List<PathProb> paths = new ArrayList<>();
       PathProb currentPath = new PathProb();
-      currentPath.addNode(0, new Rational(1, 1));
+      currentPath.addNode(0, Rational.ONE);
       visit(m, 0, new int[m.length], t, currentPath, paths, totalEvents);
-      //paths.stream().forEach(PathProb::reduceProbabilities);
 
-      Rational sum = new Rational(0, 1);
-      for (PathProb p : paths) {
-        sum = Rational.sum(sum, p.calculatePathProbability());
-      }
-      sum = sum.simplify();
-      // lcm(a,b,c) = lcm(lcm(a,b),c)
-      lcm = lcm(lcm, sum.d);
+      final Rational sum = paths.stream().map(PathProb::calculatePathProbability)
+          .reduce(Rational.ZERO, Rational::sum)
+          .simplify();
+
+      lcm = lcm(lcm, sum.d);  // lcm(a,b,c) = lcm(lcm(a,b),c)
       terminalProbabilities.add(sum);
-
-      System.out.println("Paths to : " + t + " : " + paths + " - Sum: " + sum);
-
     }
-    System.out.println("Terminal probabilities: "+terminalProbabilities);
-    System.out.println("lcm: "+lcm);
+    // System.out.println("Terminal probabilities: " + terminalProbabilities);
+    // System.out.println("lcm: " + lcm);
 
     int i = 0;
-    for(Rational r : terminalProbabilities){
-      if(r.n ==0){
+    final int[] probabilities = new int[terminals.size() + 1];
+
+    for (Rational r : terminalProbabilities) {
+      if (r.n == 0) {
         probabilities[i] = 0;
-      }
-      else if(r.d == lcm){
+      } else if (r.d == lcm) {
         probabilities[i] = r.n;
-      }else{
+      } else {
         probabilities[i] = r.n * (lcm / r.d);
       }
       i++;
     }
-
     probabilities[i] = lcm;
     System.out.println("Probabilities: " + Arrays.toString(probabilities));
 
     return probabilities;
   }
 
-  public static boolean visit(int[][] arr, int node, int[] visited, int target, PathProb currentPath, List<PathProb> paths, int[] totalEvents) {
+  static void visit(int[][] arr, int node, int[] visited, int target, PathProb currentPath, List<PathProb> paths, int[] totalEvents) {
 
     //System.out.println("Current Path: " + currentPath);
     visited[node] = visited[node] + 1;
     if (node == target) {
       paths.add(PathProb.from(currentPath));
       //System.out.println("Found Path! : " + currentPath);
-      return true;
+      return;
     }
 
     for (int i = 0; i < arr[node].length; i++) {
       int childProb = arr[node][i];
-      Rational probability = new Rational(childProb, totalEvents[node]);
-      int child = i;
-      if (childProb > 0 && (visited[child] == 0 || visited[child] == 1)) {
-        currentPath.addNode(child, probability);
-        //System.out.println("Adding " + child + " Prob: " + probability);
-        visit(arr, child, Arrays.copyOf(visited, visited.length), target, currentPath, paths, totalEvents);
+
+      if (childProb > 0 && (visited[i] == 0 || visited[i] == 1)) {
+        currentPath.addNode(i, new Rational(childProb, totalEvents[node]));
+        visit(arr, i, Arrays.copyOf(visited, visited.length), target, currentPath, paths, totalEvents);
         currentPath.removeLast();
-      } else if (childProb > 0 && visited[child] > 1) {
+      } else if (childProb > 0 && visited[i] > 1) {
         //Permite e identifica el ciclo.
         Rational p1 = currentPath.getProbabilities().get(currentPath.probabilities.size() - 3);
         Rational p2 = currentPath.getProbabilities().get(currentPath.probabilities.size() - 2);
         Rational act = currentPath.probabilities.remove(currentPath.probabilities.size() - 1);
-        Rational cicleMultiplier = new Rational(p1.d * p2.d, (p1.d * p2.d - p1.n * p2.n));
-        currentPath.probabilities.remove(currentPath.probabilities.size() - 1);
 
-        currentPath.probabilities.add(Rational.multiply(p2, cicleMultiplier));
+        // Sum of probabilities in a cycle is calculated as an infinite sum of convergent terms
+        Rational cycleMultiplier = new Rational(p1.d * p2.d, (p1.d * p2.d - p1.n * p2.n));
+
+        currentPath.probabilities.remove(currentPath.probabilities.size() - 1);
+        currentPath.probabilities.add(Rational.multiply(p2, cycleMultiplier));
         currentPath.probabilities.add(act);
-        System.out.println("Ciclo:: " + child + " - " + currentPath + " P1: " + p1 + " - P2: " + p2);
 
       }
     }
-    return false;
   }
-
 
 
   private static class PathProb {
@@ -128,7 +102,7 @@ public class FooBarCh4 {
     void addNode(Integer node, Rational probability) {
       this.path.add(node);
       //if (!(probability.n == 1 && probability.d == 1)) {
-        this.probabilities.add(probability.simplify());
+      this.probabilities.add(probability.simplify());
       //}
     }
 
@@ -141,12 +115,12 @@ public class FooBarCh4 {
       return probabilities;
     }
 
-    void reduceProbabilities() {
-      int denominator = this.probabilities.stream().map(r -> r.d).reduce(1, (a, b) -> a * b);
-      int numerator = this.probabilities.stream().map(r -> r.n).reduce(1, (a, b) -> a * b);
-      this.probabilities.clear();
-      this.probabilities.add(new Rational(numerator, denominator).simplify());
-    }
+//    void reduceProbabilities() {
+//      int denominator = this.probabilities.stream().map(r -> r.d).reduce(1, (a, b) -> a * b);
+//      int numerator = this.probabilities.stream().map(r -> r.n).reduce(1, (a, b) -> a * b);
+//      this.probabilities.clear();
+//      this.probabilities.add(new Rational(numerator, denominator).simplify());
+//    }
 
     Rational calculatePathProbability() {
       int denominator = this.probabilities.stream().map(r -> r.d).reduce(1, (a, b) -> a * b);
@@ -168,6 +142,9 @@ public class FooBarCh4 {
       this.n = a;
       this.d = b;
     }
+
+    static Rational ONE = new Rational(1, 1);
+    static Rational ZERO = new Rational(0, 1);
 
     static Rational sum(Rational a, Rational b) {
       return new Rational(a.n * b.d + b.n * a.d, a.d * b.d).simplify();
@@ -213,22 +190,22 @@ public class FooBarCh4 {
     return terminals;
   }
 
-  public static List<Integer> findUnreachables(int[][] m) {
-    List<Integer> unreachables = new ArrayList<>();
-    for (int i = 0; i < m[0].length; i++) {
-      boolean unreachable = true;
-      for (int j = 0; j < m.length; j++) {
-        if (m[j][i] > 0) {
-          unreachable = false;
-          break;
-        }
-      }
-      if (unreachable) {
-        unreachables.add(i);
-      }
-    }
-    return unreachables;
-  }
+//  public static List<Integer> findUnreachables(int[][] m) {
+//    List<Integer> unreachables = new ArrayList<>();
+//    for (int i = 0; i < m[0].length; i++) {
+//      boolean unreachable = true;
+//      for (int j = 0; j < m.length; j++) {
+//        if (m[j][i] > 0) {
+//          unreachable = false;
+//          break;
+//        }
+//      }
+//      if (unreachable) {
+//        unreachables.add(i);
+//      }
+//    }
+//    return unreachables;
+//  }
 
   public static int lcm(int a, int b) {
     if (a == 0 || b == 0) {

30d9b60e92dfc846af8094ab68e32e411ee45e97
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2025-08-21 21:10:32 -0500

    Longest substring no repeat, leetcode

---
+++ java/src/main/java/com/jpmanjarres/leetcode/from1to20/LongestSubsNoRepeat3.java
@@ -0,0 +1,32 @@
+package com.jpmanjarres.leetcode.from1to20;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubsNoRepeat3 {
+
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> bag = new HashSet<>();
+        int maxSize = 0;
+        int globalMax = 0;
+        int length = s.length();
+        for (int i=0; i<length;i++){
+            if (globalMax> length- i){
+                break;
+            }
+            for(int j=i; j< length; j++) {
+                Character c = s.charAt(j);
+                if (!bag.contains(c)) {
+                    bag.add(c);
+                    maxSize++;
+                } else {
+                    bag.clear();
+                    globalMax = Math.max(globalMax, maxSize);
+                    maxSize = 0;
+                    break;
+                }
+            }
+        }
+        return Math.max(globalMax, maxSize);
+    }
+}
